c15ad69e6faae05edd03ee66f70356c426462786
hsy <hsy.com>
2022-11-02 18:49:06 +0800

    this is  form hot-fix

--- src/Demo00/gitTest.java
+++ src/Demo00/gitTest.java
@@ -12,5 +12,6 @@ public class gitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello hsy");
+        System.out.println("this is hot-fix");
         }
 }
