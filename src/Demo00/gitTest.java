53447fd00d146241e0f15ec4b248903875c8bdc3
hsy <hsy.com>
2022-11-02 17:05:22 +0800

    hello git

--- src/Demo00/gitTest.java
+++ src/Demo00/gitTest.java
@@ -4,9 +4,12 @@ package Demo00;
  * @Author: hsy
  * @Date: 2022/11/02/17:02
  * @Description:
+ * hello git 那一行是已经提交过了
+ * hello git2的输出语句，一写上就变蓝色了
  */
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
         }
 }
