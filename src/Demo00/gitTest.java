d76f0af3dc9c205aab60350ff3e2f6fb6d4059b5
hsy <hsy.com>
2022-11-02 17:03:47 +0800

    hello git

---
+++ src/Demo00/gitTest.java
@@ -0,0 +1,12 @@
+package Demo00;
+
+/**
+ * @Author: hsy
+ * @Date: 2022/11/02/17:02
+ * @Description:
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        }
+}
